ebc248932b8bae4b7e47bfeb82494bf4f4c6678f
Bernard Maina <hazardmburu10@gmail.com>
2025-02-20 21:12:28 +0300

    Generics and ref

---
+++ Generics/Utilities.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Create a regular (non-generic) class called Utilities that has these generic methods:
+ * Swap<T> - takes two parameters of type T and swaps their values
+ * PrintArray<T> - takes an array of type T and prints all elements
+ * Find<T> - takes an array of type T and an item to find, returns the index of the item (-1 if not found)
+ * Some hints:
+ * - Generic methods use the <T> syntax right before the return type
+ * - You'll need to use the ref keyword for the Swap method to make it work
+ * - For Find, you'll need to use Equals method to compare objects
+ */
+ 
+class Utilities
+{
+    public void Swap<T>(ref T param1, ref T param2)
+    {
+        (param1, param2) = (param2, param1);
+    }
+
+    public void PrintArray<T>(T[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Console.WriteLine($"{item}");
+        }
+
+        Console.WriteLine();
+    }
+    
+    public int Find<T>(T[] arr, T item)
+    {
+        for (var i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Equals(item))
+            {
+                return i;
+            }
+        }
+        return -1; // Not found
+    }
+}
---
+++ ref/Ref.cs
@@ -0,0 +1,21 @@
+﻿// The ref keyword is used to pass a variable by reference. This means that any modifications inside the method will reflect outside.
+//
+// Key Rules of ref:
+// The variable must be initialized before being passed to the method.
+//     The method receives a reference to the original variable, not a copy.
+//     Any modifications inside the method affect the original variable.
+//     It can be used in method parameters and return values.
+
+class Ref
+{
+    public void ModifyValue(ref int x)
+    {
+        x += 10;
+    }
+
+    public int GetLargest(ref int a, ref int b)
+    {
+        var getLargest = a > b ? a : b;
+        return getLargest;
+    }
+}
